2f3a6e1406d4c55939a149533f0ec4fac970e0c5
Stephen Roth <designbystephen@gmail.com>
2020-08-16 03:33:29 -0500

    - added change that will force clean checkout during install script - added error handling. timeouts and failure cases to speedtest script

--- src/speedtest.py
+++ src/speedtest.py
@@ -8,21 +8,31 @@ home = str(Path.home())
 appName = 'raspi-speed-test'
 outfile = home + '/local/' + appName + '/reports/speedtest.csv'
 
+print('[INFO] Attempting Speedtest')
+
 response = subprocess.Popen(
-    '/usr/local/bin/speedtest-cli --simple',
+    '/usr/local/bin/speedtest-cli --simple --timeout 30',
     shell=True,
     stdout=subprocess.PIPE
 ).stdout.read().decode('utf-8')
 
-ping = re.findall('Ping:\s(.*?)\s', response, re.MULTILINE)
-download = re.findall('Download:\s(.*?)\s', response, re.MULTILINE)
-upload = re.findall('Upload:\s(.*?)\s', response, re.MULTILINE)
+if response <= 1:
+    print('[SUCCESS] Speedtest successful')
+    ping = re.findall('Ping:\s(.*?)\s', response, re.MULTILINE)
+    download = re.findall('Download:\s(.*?)\s', response, re.MULTILINE)
+    upload = re.findall('Upload:\s(.*?)\s', response, re.MULTILINE)
 
-ping = ping[0].replace(',', '.')
-download = download[0].replace(',', '.')
-upload = upload[0].replace(',', '.')
+    ping = ping[0].replace(',', '.')
+    download = download[0].replace(',', '.')
+    upload = upload[0].replace(',', '.')
+elif:
+    print('[FAILURE] Speedtest failure')
+    ping = -1
+    download = -1
+    upload = -1
 
 try:
+    print('[INFO] Writing results to csv file')
     f = open(outfile, 'a+')
     if os.stat(outfile).st_size == 0:
         f.write('Date,Time,Ping (ms),Download (Mbit/s),Upload (Mbit/s)\r\n')
@@ -30,4 +40,4 @@ try:
 except:
     pass
 
-print('[SUCCESS] report available here', outfile)
+print('[SUCCESS] report available @ ', outfile)
