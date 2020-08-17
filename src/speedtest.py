fe5918888fff226b529025fe8df39b47f8715831
Stephen Roth <designbystephen@gmail.com>
2020-08-16 22:49:05 -0500

    - Modifyied python speedtest script to cover more error cases

--- src/speedtest.py
+++ src/speedtest.py
@@ -10,36 +10,48 @@ outfile = home + '/local/' + appName + '/reports/speedtest.csv'
 
 print('[INFO] Attempting Speedtest')
 
-response = subprocess.Popen(
-    '/usr/local/bin/speedtest-cli --simple --timeout 30',
-    shell=True,
-    stdout=subprocess.PIPE
-)
-
-results = response.stdout.read().decode('utf-8')
-
-if int(response.returncode or 0) <= 1:
-    print('[SUCCESS] Speedtest successful')
-    ping = re.findall('Ping:\s(.*?)\s', results, re.MULTILINE)
-    download = re.findall('Download:\s(.*?)\s', results, re.MULTILINE)
-    upload = re.findall('Upload:\s(.*?)\s', results, re.MULTILINE)
-
-    ping = ping[0].replace(',', '.')
-    download = download[0].replace(',', '.')
-    upload = upload[0].replace(',', '.')
-else:
-    print('[FAILURE] Speedtest failure')
+def get_speedtest_results():
     ping = -1
     download = -1
     upload = -1
 
+    try:
+        response = subprocess.Popen(
+            '/usr/local/bin/speedtest-cli --simple --timeout 30',
+            shell=True,
+            stdout=subprocess.PIPE
+        )
+        results = response.stdout.read().decode('utf-8')
+
+        if int(response.returncode or 0) <= 1:
+            print('[SUCCESS] Speedtest successful')
+            ping = re.findall('Ping:\s(.*?)\s', results, re.MULTILINE)
+            download = re.findall('Download:\s(.*?)\s', results, re.MULTILINE)
+            upload = re.findall('Upload:\s(.*?)\s', results, re.MULTILINE)
+
+            ping = ping[0].replace(',', '.')
+            download = download[0].replace(',', '.')
+            upload = upload[0].replace(',', '.')
+        else:
+            print('[FAILURE] Speedtest failure')
+    except Exception as e:
+        print('[FAILURE] Speedtest CLI failure', e)
+    finally:
+        return ping, download, upload
+
 try:
     print('[INFO] Writing results to csv file')
+
     f = open(outfile, 'a+')
     if os.stat(outfile).st_size == 0:
         f.write('Date,Time,Ping (ms),Download (Mbit/s),Upload (Mbit/s)\r\n')
-    f.write('{},{},{},{},{}\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), ping, download, upload))
-except:
-    pass
 
-print('[SUCCESS] report available @ ', outfile)
+    ping, download, upload = get_speedtest_results()
+
+    f.write('{},{},{},{},{}\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), ping, download, upload))
+
+    print('[SUCCESS] Report written to file ')
+except Exception as e:
+    print('[FAILURE] Error writting report results to file', e)
+finally:
+    print('[INFO] Report available @ ', outfile)
